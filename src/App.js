7af7c9bc1f752453c2eb907a601a4d328a1655b8
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-10-20 08:20:10 +0700

    feature: created initial navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          COMING SOON...
-          <br />
-          Currently, we are building something great
-        </p>
-      </header>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import HomePage from "./containers/HomePage";
+import Login from "./containers/Login";
+import MoviePage from "./containers/MoviePage";
+import NotFound from "./containers/NotFound";
+import ProfilesPage from "./containers/ProfilesPage";
+import Register from "./containers/Register";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./themes/theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<NotFound />}>
+      <Route errorElement={<NotFound />} />
+      <Route index element={<HomePage />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="movies/:movieId" element={<MoviePage />} />
+      <Route path="profiles/:profileId" element={<ProfilesPage />} />
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
