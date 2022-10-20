20460da75e87853eeb96f4dc2e726c48ee8f2d93
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-10-20 18:16:25 +0700

    feature: created initial movies list component

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,8 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const MoviesList = () => {
-  return <div>MoviesList</div>;
+  return <Box>MoviesList</Box>;
 };
 
 export default MoviesList;
